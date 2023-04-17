1250596043c2a62e31749936ab48f05828f7302f
Errera11 <forvitinn@mail.ru>
2023-04-17 21:19:07 +0300

    init

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <div className={'form'}>
+                <form>
+                    <h3>A simple form</h3>
+                    <input type={'text'}/>
+                    <button>Send</button>
+                </form>
+            </div>
+            <div className={'messages'}>
+
+            </div>
+        </div>
+    );
+}
+
+export default App;
